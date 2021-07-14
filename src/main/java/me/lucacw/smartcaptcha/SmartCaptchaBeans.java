aeacd9ed14dc3b88d24655dc8a85aacb88cabb70
LucaCW <email.office45@yahoo.com>
2021-07-15 00:39:17 +0200

    Add bot base

--- src/main/java/me/lucacw/smartcaptcha/SmartCaptchaBeans.java
+++ src/main/java/me/lucacw/smartcaptcha/SmartCaptchaBeans.java
@@ -1,8 +1,16 @@
 package me.lucacw.smartcaptcha;
 
+import lombok.SneakyThrows;
 import me.lucacw.smartcaptcha.config.JsonConfig;
+import me.lucacw.smartcaptcha.config.imp.BotSettingsConfig;
 import me.lucacw.smartcaptcha.config.imp.MySQLDatabaseConfig;
 import me.lucacw.smartcaptcha.database.AsyncMySQL;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
+import net.dv8tion.jda.api.sharding.ShardManager;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +21,43 @@ import org.springframework.stereotype.Component;
 @Component
 public final class SmartCaptchaBeans {
 
-    private static final JsonConfig<MySQLDatabaseConfig> MYSQL_CONFIG = new JsonConfig(MySQLDatabaseConfig.class, "mysql.json");
+    private static final JsonConfig<MySQLDatabaseConfig> MYSQL_CONFIG = new JsonConfig<>(MySQLDatabaseConfig.class, "mysql.json");
+    private static final JsonConfig<BotSettingsConfig> BOT_SETTINGS_CONFIG = new JsonConfig<>(BotSettingsConfig.class, "bot.json");
+
+    private final MySQLDatabaseConfig mySQLDatabaseConfig = MYSQL_CONFIG.getConfig();
+    private final BotSettingsConfig botSettingsConfig = BOT_SETTINGS_CONFIG.getConfig();
 
     private final AsyncMySQL mysql = new AsyncMySQL(MYSQL_CONFIG.getConfig());
+
+    private final ShardManager shardManager;
+
+    @SneakyThrows
+    public SmartCaptchaBeans(ApplicationContext applicationContext) {
+        DefaultShardManagerBuilder builder = DefaultShardManagerBuilder.createDefault(this.botSettingsConfig.getToken());
+        builder.enableIntents(GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MESSAGES);
+        this.shardManager = builder.build();
+        this.shardManager.setStatus(this.botSettingsConfig.getOnlineStatus());
+        this.shardManager.setActivity(Activity.of(this.botSettingsConfig.getActivityType(), this.botSettingsConfig.getActivity()));
+    }
 
     @Bean
     public AsyncMySQL mysql() {
         return this.mysql;
+    }
+
+    @Bean
+    public ShardManager shardManager() {
+        return this.shardManager;
+    }
+
+    @Bean
+    public MySQLDatabaseConfig mySQLDatabaseConfig() {
+        return this.mySQLDatabaseConfig;
+    }
+
+    @Bean
+    public BotSettingsConfig botSettingsConfig() {
+        return this.botSettingsConfig;
     }
 
 }
---
+++ src/main/java/me/lucacw/smartcaptcha/config/imp/BotSettingsConfig.java
@@ -0,0 +1,35 @@
+package me.lucacw.smartcaptcha.config.imp;
+
+import lombok.Data;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.internal.handle.GuildSetupController;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Luca R. at 15.07.2021
+ * @project smart-captcha
+ */
+@Data
+public class BotSettingsConfig {
+
+    private String token;
+
+    private Activity.ActivityType activityType;
+    private String activity;
+    private OnlineStatus onlineStatus;
+
+    private List<String> admins;
+
+    public BotSettingsConfig() {
+        this.token = "YOUR_TOKEN";
+        this.activity = "smart-captcha - your captcha bot.";
+        this.activityType = Activity.ActivityType.DEFAULT;
+        this.onlineStatus = OnlineStatus.ONLINE;
+
+        this.admins = Collections.singletonList("634056471746707468");
+    }
+
+}
